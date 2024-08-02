5ece429195d1dd58f9870a13e3393352b08fca49
moonia <ramunas@inultimo.lt>
2024-08-02 20:07:11 +0300

    category page update

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,6 @@
-command = '/home/tvparkas/myenv/bin/gunicorn'
-pythonpath = '/home/tvparkas/store'
 bind = '127.0.0.1:8000'
 workers = 3
+user = 'root'
+group = 'root'
+accesslog = '/home/tvparkas/store/logs/gunicorn-access.log'
+errorlog = '/home/tvparkas/store/logs/gunicorn-error.log'
